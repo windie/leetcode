8be7a536291e8766637a28c9b43e9e8f1c440c81
xiaoyan <linxiaoyan18@gmail.com>
2013-10-15 11:12:19 +0800

    solutions updated

--- src/Reverse_Linked_List_II.h
+++ src/Reverse_Linked_List_II.h
@@ -10,107 +10,38 @@ using namespace std;
  * };
  */
 class Solution {
-    void reverse(ListNode * head, ListNode *tail){
-        ListNode* cur = head, *pre = NULL, *restail = NULL;
-        while(cur != tail){
-            if(pre==NULL){
-                pre = cur;
-                restail = cur;
-                cur = cur->next;
-            }else{
-                ListNode *next = cur->next;
-                cur->next = pre;
-                pre = cur;
-                cur = next;
-            }
-        }
-        cur->next = pre;
-        head = cur;
-        tail = restail;
-        tail->next = NULL;
-    }
 public:
     ListNode *reverseBetween(ListNode *head, int m, int n) {
-        if(head == NULL || m==n){
-            return head;
-        }
-        int n1 = m-1, n2 = m, n3=n, n4 = n+1;
-        ListNode *r1 = NULL, *r2 =NULL, *r3 = NULL, *r4 = NULL;
-        ListNode *p = head;
-        int i = 1;
-        while(p){
-            if(i==n1){
-                r1 = p;
-            }else if(i==n2){
-                r2=p;
-            }else if(i==n3){
-                r3= p;
-            }else if(i == n4){
-                r4 = p;
-                break;
-            }
-            p = p->next;
-            i++;
-        }
-        reverse(r2, r3);
-        if(r1 == NULL && r4 == NULL){
-            return r2;
-        }else if(r1 == NULL){
-            r3->next = r4;
-            return r2;
-        }else if(r4 == NULL){
-            r1->next = r2;
-            return head;
-        }else{
-            r1->next = r2;
-            r3->next = r4;
-            return head;
-        }
+    	if(head==NULL){
+    		return NULL;
+    	}
+    	ListNode *cur = head;
+    	ListNode *connection1_left = NULL, *connection1_right = NULL;
+    	int i=1;
+    	while(i<m){
+    		connection1_left = cur;
+    		cur = cur->next;
+    		i++;
+    	}
+    	if(i==n){
+    		return head;
+    	}
+    	ListNode *pre = cur;
+    	connection1_right = cur;
+    	cur = cur->next;
+    	ListNode *next = NULL;
+    	while(i<n){
+    		next = cur->next;
+    		cur->next = pre;
+    		pre = cur;
+    		cur = next;
+    		i++;
+    	}
+    	connection1_right->next = next;
+    	if(connection1_left==NULL){
+    		return pre;
+    	}
+    	connection1_left->next = pre;
+    	return head;
     }
 };
-
-
-///**
-// * Definition for singly-linked list.
-// * struct ListNode {
-// *     int val;
-// *     ListNode *next;
-// *     ListNode(int x) : val(x), next(NULL) {}
-// * };
-// */
-//class Solution {
-//public:
-//    ListNode *reverseBetween(ListNode *head, int m, int n) {
-//    	if(head==NULL){
-//    		return NULL;
-//    	}
-//    	ListNode *cur = head;
-//    	ListNode *connection1_left = NULL, *connection1_right = NULL;
-//    	int i=1;
-//    	while(i<m){
-//    		connection1_left = cur;
-//    		cur = cur->next;
-//    		i++;
-//    	}
-//    	if(i==n){
-//    		return head;
-//    	}
-//    	ListNode *pre = cur;
-//    	connection1_right = cur;
-//    	cur = cur->next;
-//    	ListNode *next = NULL;
-//    	while(i<n){
-//    		next = cur->next;
-//    		cur->next = pre;
-//    		pre = cur;
-//    		cur = next;
-//    		i++;
-//    	}
-//    	connection1_right->next = next;
-//    	if(connection1_left==NULL){
-//    		return pre;
-//    	}
-//    	connection1_left->next = pre;
-//    	return head;
-//    }
-//};
--- test/Reverse_Linked_List_II_test.cc
+++ test/Reverse_Linked_List_II_test.cc
@@ -2,12 +2,3 @@
 #include "Reverse_Linked_List_II.h"
 #include <gtest/gtest.h>
 
-TEST(test1, reverseBetween){
-	Solution s;
-	ListNode *head = new ListNode(3);
-	ListNode *n2 = new ListNode(5);
-	head->next = n2;
-	s.reverseBetween(head, 1, 2);
-	EXPECT_EQ(5, head->val);
-	EXPECT_EQ(3, head->next->val);
-}
