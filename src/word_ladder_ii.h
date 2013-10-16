299f5c4aefb59bf1fd928b88cebc03dd37ed3d48
xiaoyan <linxiaoyan18@gmail.com>
2013-10-16 16:29:37 +0800

    solution for Gas station

--- src/word_ladder_ii.h
+++ src/word_ladder_ii.h
@@ -1,52 +1,122 @@
+//class Solution {
+//public:
+//	vector<vector<string> > findLadders(string start, string end,
+//			unordered_set<string> &dict) {
+//		pathes.clear();
+//		dict.insert(start);
+//		dict.insert(end);
+//		vector < string > prev;
+//		unordered_map<string, vector<string> > traces;
+//		for (unordered_set<string>::const_iterator citr = dict.begin();
+//				citr != dict.end(); citr++) {
+//			traces[*citr] = prev;
+//		}
+//
+//		vector < unordered_set<string> > layers(2);
+//		int cur = 0;
+//		int pre = 1;
+//		layers[cur].insert(start);
+//		while (true) {
+//			cur = !cur;
+//			pre = !pre;
+//			for (unordered_set<string>::const_iterator citr =
+//					layers[pre].begin(); citr != layers[pre].end(); citr++)
+//				dict.erase(*citr);
+//			layers[cur].clear();
+//			for (unordered_set<string>::const_iterator citr =
+//					layers[pre].begin(); citr != layers[pre].end(); citr++) {
+//				for (int n = 0; n < (*citr).size(); n++) {
+//					string word = *citr;
+//					int stop = word[n] - 'a';
+//					for (int i = (stop + 1) % 26; i != stop; i = (i + 1) % 26) {
+//						word[n] = 'a' + i;
+//						if (dict.find(word) != dict.end()) {
+//							traces[word].push_back(*citr);
+//							layers[cur].insert(word);
+//						}
+//					}
+//				}
+//			}
+//			if (layers[cur].size() == 0)
+//				return pathes;
+//			if (layers[cur].count(end))
+//				break;
+//		}
+//		vector < string > path;
+//		buildPath(traces, path, end);
+//
+//		return pathes;
+//	}
+//
+//private:
+//	void buildPath(unordered_map<string, vector<string> > &traces,
+//			vector<string> &path, const string &word) {
+//		if (traces[word].size() == 0) {
+//			path.push_back(word);
+//			vector < string > curPath = path;
+//			reverse(curPath.begin(), curPath.end());
+//			pathes.push_back(curPath);
+//			path.pop_back();
+//			return;
+//		}
+//
+//		const vector<string> &prevs = traces[word];
+//		path.push_back(word);
+//		for (vector<string>::const_iterator citr = prevs.begin();
+//				citr != prevs.end(); citr++) {
+//			buildPath(traces, path, *citr);
+//		}
+//		path.pop_back();
+//	}
+//
+//	vector<vector<string> > pathes;
+//};
+
 class Solution {
+
 public:
 	vector<vector<string> > findLadders(string start, string end,
 			unordered_set<string> &dict) {
 		vector < vector<string> > res;
 		vector < string > graph;
 		vector<int> path;
-		unordered_set < string > used;
 		graph.push_back(start);
-		used.insert(start);
+		dict.erase(start);
 		path.push_back(-1);
 		int begin = 0, size = 1;
 		bool isfind = false;
+		int numLevel = 1;
 		while (begin < size && !isfind) {
+			numLevel++;
 			for (int i = begin; i < size; i++) {
-				string curword = graph[i];
-
-				for (int j = 0; j < curword.size(); j++) {
+				for (int j = 0; j < graph[i].size(); j++) {
 					for (int ch = 'a'; ch <= 'z'; ch++) {
-						string newword = curword;
-						if (newword[j] != ch) {
-							newword[j] = ch;
-							// add path
-							if (newword == end) {
-								vector < string > shortestPath;
-								vector < string > each;
-								each.push_back(newword);
-								int k = i;
-								while (k >= 0) {
-									each.push_back(graph[k]);
-									k = path[k];
-								}
-								shortestPath.assign(each.rbegin(), each.rend());
-								isfind = true;
-								res.push_back(shortestPath);
+						if (ch == graph[i][j]) {
+							continue;
+						}
+						string newword = graph[i];
+						newword[j] = ch;
+						if (newword == end) {
+							vector < string > shortestPath(numLevel);
+							int k = i, index = numLevel - 1;
+							shortestPath[index--] = newword;
+							while (k >= 0) {
+								shortestPath[index--] = graph[k];
+								k = path[k];
 							}
-							// new graph
-							if (dict.find(newword) != dict.end()
-									&& used.find(newword) == used.end()) {
-								graph.push_back(newword);
-								path.push_back(i);
-							}
+							isfind = true;
+							res.push_back(shortestPath);
+						}
+						if (dict.find(newword) != dict.end()) {
+							graph.push_back(newword);
+							path.push_back(i);
 						}
 					}
 				}
 			}
 			begin = size;
 			for (int ii = size; ii < graph.size(); ii++) {
-				used.insert(graph[ii]);
+				dict.erase(graph[ii]);
 			}
 			size = graph.size();
 		}
