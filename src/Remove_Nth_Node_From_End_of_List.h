57d92ee8e5afc887c2283425a43f01b64607be5b
Xiaoyan Lin <linxiaoyan18@gmail.com>
2015-04-06 20:46:23 +0800

    add remove nth node from end of list

--- src/Remove_Nth_Node_From_End_of_List.h
+++ src/Remove_Nth_Node_From_End_of_List.h
@@ -1,24 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
     ListNode *removeNthFromEnd(ListNode *head, int n) {
-        if(head == NULL){
-            return NULL;
+        if(n == 0){
+            return head;
         }
-        ListNode *fast = head;
-        for(int i = 0; i<n; i++){
-            if(fast){
-                fast = fast->next;
-            }
-        }
-        if(fast == NULL){
-            return head->next;
-        }
-        ListNode *slow=head;
-        while(fast && fast->next){
-            slow = slow->next;
+        ListNode *fast = head, *slow = head;
+        int i = 0;
+        for(i = 0; i<=n && fast != NULL; i++){
             fast = fast->next;
         }
-        if(slow->next){
+        if(fast == NULL && i<n+1){
+            return head->next;
+        }
+        while(fast != NULL){
+            fast = fast->next;
+            slow = slow->next;
+        }
+        if(slow->next != NULL){
             slow->next = slow->next->next;
         }else{
             slow->next = NULL;
@@ -26,6 +32,35 @@ public:
         return head;
     }
 };
+
+//class Solution {
+//public:
+//    ListNode *removeNthFromEnd(ListNode *head, int n) {
+//        if(head == NULL){
+//            return NULL;
+//        }
+//        ListNode *fast = head;
+//        for(int i = 0; i<n; i++){
+//            if(fast){
+//                fast = fast->next;
+//            }
+//        }
+//        if(fast == NULL){
+//            return head->next;
+//        }
+//        ListNode *slow=head;
+//        while(fast && fast->next){
+//            slow = slow->next;
+//            fast = fast->next;
+//        }
+//        if(slow->next){
+//            slow->next = slow->next->next;
+//        }else{
+//            slow->next = NULL;
+//        }
+//        return head;
+//    }
+//};
 
 ///**
 // * Definition for singly-linked list.
