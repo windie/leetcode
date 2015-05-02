e7a8389808303a55e62ace4a901153ae35cc1ed0
Xiaoyan Lin <linxiaoyan18@gmail.com>
2015-05-02 14:23:38 +0800

    permutations

--- src/permutations.h
+++ src/permutations.h
@@ -1,30 +1,56 @@
-#include<algorithm>
-using namespace std;
-
 class Solution {
-	vector<vector<int> > recursive(vector<vector<int> > &cur, int index, vector<int> &num){
-		if(index == num.size()){
-			return cur;
-		}
-		vector< vector<int> > res;
-		for(int i= 0; i<cur.size(); i++){
-			for(int j=0; j<=cur[i].size(); j++){
-				vector<int> tmp = cur[i];
-				tmp.insert(tmp.begin() + j, 1, num[index]);
-				res.push_back(tmp);
-			}
-		}
-		return recursive(res, index+1, num);
-	}
+    void rec(vector<vector<int> > &res, int index, vector<int> & nums){
+        if(index == nums.size()){
+            return;
+        }
+        vector<vector<int> > cur;
+        for(int i = 0; i<res.size(); i++){
+            for(int j = 0; j<=res[i].size(); j++){
+                vector<int> tmp = res[i];
+                tmp.insert(tmp.begin() + j, 1, nums[index]);
+                cur.push_back(tmp);
+            }
+        }
+        res = cur;
+        rec(res, index+1, nums);
+    }
 public:
-	vector<vector<int> > permute(vector<int> &num) {
-		vector<vector<int> > res;
-		vector<int> tmp;
-		res.push_back(tmp);
-		res = recursive(res, 0, num);
-		return res;
-	}
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int> tmp;
+        vector<vector<int> > res;
+        res.push_back(tmp);
+        rec(res, 0, nums);
+        return res;
+    }
 };
+
+//#include<algorithm>
+//using namespace std;
+//
+//class Solution {
+//	vector<vector<int> > recursive(vector<vector<int> > &cur, int index, vector<int> &num){
+//		if(index == num.size()){
+//			return cur;
+//		}
+//		vector< vector<int> > res;
+//		for(int i= 0; i<cur.size(); i++){
+//			for(int j=0; j<=cur[i].size(); j++){
+//				vector<int> tmp = cur[i];
+//				tmp.insert(tmp.begin() + j, 1, num[index]);
+//				res.push_back(tmp);
+//			}
+//		}
+//		return recursive(res, index+1, num);
+//	}
+//public:
+//	vector<vector<int> > permute(vector<int> &num) {
+//		vector<vector<int> > res;
+//		vector<int> tmp;
+//		res.push_back(tmp);
+//		res = recursive(res, 0, num);
+//		return res;
+//	}
+//};
 
 //class Solution {
 //	void swap(vector<int> &num, int i, int k) {
